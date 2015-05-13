4b54431b60a22df1ad39343b10df1100e9cf0da9
krispychoi <christina@mozilla.com>
2015-05-13 10:26:46 -0700

    small change

--- ciproject/clean.R
+++ ciproject/clean.R
@@ -17,12 +17,12 @@ predDates <- seq(as.Date(predRange$start), as.Date(predRange$end), "month")
 daysSinceSampleCreated <- abs(predDates - currentInfoDate)
 tableCI <- data.table(predDates, daysSinceSampleCreated)
 tableCI$predDates <- as.Date(tableCI$predDates) - months(1)
-mauInfo <- data.table(mau.est = mauRange[tableCI$daysSinceSampleCreated,][[1]], mau.lb = mauRange[tableCI$daysSinceSampleCreated,][[2]]*1.03, mau.ub = mauRange[tableCI$daysSinceSampleCreated,][[3]]*0.97)
-srchInfo <- data.table(srch.est = srchRange[tableCI$daysSinceSampleCreated,][[1]], srch.lb = srchRange[tableCI$daysSinceSampleCreated,][[2]]*1.03, srch.ub = srchRange[tableCI$daysSinceSampleCreated,][[3]]*0.97)
-secInfo <- data.table(sec.est = secRange[tableCI$daysSinceSampleCreated,][[1]], sec.lb = secRange[tableCI$daysSinceSampleCreated,][[2]]*1.03, sec.ub = secRange[tableCI$daysSinceSampleCreated,][[3]]*0.97)
+mauInfo <- data.table(mau_est = mauRange[tableCI$daysSinceSampleCreated,][[1]], mau_lb = mauRange[tableCI$daysSinceSampleCreated,][[2]]*1.03, mau_ub = mauRange[tableCI$daysSinceSampleCreated,][[3]]*0.97)
+srchInfo <- data.table(srch_est = srchRange[tableCI$daysSinceSampleCreated,][[1]], srch_lb = srchRange[tableCI$daysSinceSampleCreated,][[2]]*1.03, srch_ub = srchRange[tableCI$daysSinceSampleCreated,][[3]]*0.97)
+secInfo <- data.table(sec_est = secRange[tableCI$daysSinceSampleCreated,][[1]], sec_lb = secRange[tableCI$daysSinceSampleCreated,][[2]]*1.03, sec_ub = secRange[tableCI$daysSinceSampleCreated,][[3]]*0.97)
 finalTable <- data.table(tableCI, mauInfo, srchInfo, secInfo)
 
-write.table(finalTable, file = "~/output/finalTable.csv", sep = ",", row.names = FALSE)
+write.table(finalTable, file = "~/output/confidence_interval .csv", sep = ",", row.names = FALSE)
 
 
 
